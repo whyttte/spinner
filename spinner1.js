bb360cf521fa81720aebb489aee39948ebc6f300
whyttte <fogboruche@yahoo.com>
2020-07-28 23:43:35 +0000

    my first spinner

---
+++ spinner1.js
@@ -0,0 +1,32 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 600);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1200);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1800);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 2400);
+
+// ... fill in the rest yourself ...
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\/   '); 
+}, 3000);
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\-   '); 
+}, 3600);
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\|   '); 
+}, 4200);
